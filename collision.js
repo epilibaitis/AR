9a41d489fc8e62cec4b3f63555cc220dc872c821
epilibaitis <epilibaitis@users.noreply.github.com>
2020-05-31 03:56:03 +0100

    a

--- collision.js
+++ collision.js
@@ -1,6 +1,6 @@
     var laser = document.querySelector('#prism');
     laser.addEventListener('collide', function(e) 
     {
-        laser.setAttribute('color',"#ffffff")
+        laser.setAttribute('color',"#ff0000")
     });
 
